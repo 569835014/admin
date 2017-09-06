987a53ab5751b22fdfe242c0775641192317fae5
e <569835014@qq.com>
2017-09-06 17:51:40 +0800

    koa项目初始化

--- server/config/index.js
+++ server/config/index.js
@@ -5,7 +5,7 @@ export default {
     db: "mongodb://localhost:27018/admin",
     wechat: {
         token: 'cdmacctvccav',
-        appId: 'wx3bd4658fa7c863d3',
-        appSecret: '2b00f20a3c557d6812992b0e3562f3f1'
+        appId: '',
+        appSecret: ''
     }
 }
